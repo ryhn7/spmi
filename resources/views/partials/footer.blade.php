116e78df657dff4d8de27c42b89a66a4aa2304b0
Rayhan Illyas <rayhan205732@gmail.com>
2023-10-12 17:07:59 +0700

    feat: done add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,114 +1,142 @@
     <!-- Footer -->
-    <section class="bg-[#03051e] sectionSize">
+    <section class="bg-[#03051e] footerSize">
         <div class="w-full">
-            <div class="flex flex-col">
-                <div class="">
-                    <div class="flex justify-evenly">
-                        <div class="w-1/3 flex flex-col items-center">
-                            <h2 class="font-open font-bold text-lg text-white pb-4">Alamat</h2>
+            <div class="flex flex-row justify-center pb-14 px-24">
+                <div class="w-1/3 flex flex-col items-center">
+                    <h2 class="font-open font-bold text-lg text-white pb-4">Alamat</h2>
+                    <ol>
+                        <li class="font-open font-normal text-center mb-2">
+                            <p class="text-[#8996a0]">Gedung Acintya Prasada Lantai 2,<br> Fakultas
+                                Sains dan
+                                Matematika <br>
+                                Jalan Prof. Jacob Rais, Tembalang <br> Semarang – 50275, Jawa Tengah, <br>
+                                Indonesia</p>
+                        </li>
+                        <li class="font-open font-normal text-center">
+                            <a href="https://maps.app.goo.gl/eq5TfDigMQkCvbhF7" target="_blank"
+                                class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">Lokasi</a>
+                        </li>
+                    </ol>
+                </div>
+                <div class="w-1/3 flex flex-col items-center">
+                    <h2 class="font-open font-bold text-lg text-white pb-4">Tautan Eksternal</h2>
+                    <div class="flex w-full">
+                        <div class="w-1/2 text-center">
                             <ol>
-                                <li class="font-open font-normal text-center">
-                                    <p class="text-[#8996a0] pb-2">Gedung Acintya Prasada Lantai 2, Fakultas Sains dan
-                                        Matematika
-                                        Jalan Prof. Jacob Rais, Tembalang Semarang – 50275, Jawa Tengah, Indonesia</p>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://www.undip.ac.id/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">Universitas
+                                        Diponegoro</a>
                                 </li>
-                                <li class="font-open font-normal text-center">
-                                    <a href="" class="text-[#8996a0] pb-2">Lokasi</a>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://fsm.undip.ac.id/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">Fakultas
+                                        Sains dan
+                                        Matematika</a>
+                                </li>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://bio.fsm.undip.ac.id/v1/en/home-2/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">S1
+                                        Biologi</a>
+                                </li>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://mbio.fsm.undip.ac.id/v1/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">S2
+                                        Biologi</a>
+                                </li>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://biotek.fsm.undip.ac.id/v1/en/home-2/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">S1
+                                        Bioteknologi</a>
+                                </li>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://fisika.fsm.undip.ac.id/v2/en/front-page/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">S1
+                                        Fisika</a>
+                                </li>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://mf.fsm.undip.ac.id/en/home/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">S2
+                                        Fisika</a>
                                 </li>
                             </ol>
                         </div>
-                        <div class="w-1/3 flex flex-col items-center">
-                            <h2 class="font-open font-bold text-lg text-white pb-4">Tautan External</h2>
-                            <div class="flex text-center">
-                                <div class="w-1/2">
-                                    <ol>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">Universitas Diponegoro</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">Fakultas Sains dan
-                                                Matematika</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">S1 Biologi</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">S2 Biologi</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">S1 Bioteknologi</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">S1 Fisika</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">S2 Fisika</a>
-                                        </li>
-                                    </ol>
-                                </div>
-                                <div class="w-1/2">
-                                    <ol>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">Profesi Fisikawan Medis</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">S1 Informatika</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">S1 Kimia</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">S2 Kimia</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">S1 Matematika</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">S2 Matematika</a>
-                                        </li>
-                                        <li class="font-open font-normal">
-                                            <a href="" class="text-[#8996a0] pb-2">S1 Statistika</a>
-                                        </li>
-                                    </ol>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="w-1/3 flex flex-col items-center">
-                            <h2 class="font-open font-bold text-lg text-white pb-4">Kontak Kami</h2>
+                        <div class="w-1/2 text-center">
                             <ol>
-                                <li class="font-open font-normal text-center">
-                                    <a href="" class="text-[#8996a0] pb-2">Phone: (024) 7474754</a>
+                                <li class="font-open font-normal mb-2">
+                                    <a href=" https://fismed.fsm.undip.ac.id/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">Profesi
+                                        Fisikawan
+                                        Medis</a>
                                 </li>
-                                <li class="font-open font-normal text-center">
-                                    <a href="" class="text-[#8996a0] pb-2">tpmf.fsm@live.undip.ac.id</a>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://if.fsm.undip.ac.id/id/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">S1
+                                        Informatika</a>
+                                </li>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="http://kimia.fsm.undip.ac.id/id/beranda-2/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">S1
+                                        Kimia</a>
+                                </li>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://mkim.fsm.undip.ac.id/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">S2
+                                        Kimia</a>
+                                </li>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://math.fsm.undip.ac.id/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">S1
+                                        Matematika</a>
+                                </li>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://mmath.fsm.undip.ac.id/new/en/home/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">S2
+                                        Matematika</a>
+                                </li>
+                                <li class="font-open font-normal mb-2">
+                                    <a href="https://stat.fsm.undip.ac.id/v1/" target="_blank"
+                                        class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">S1
+                                        Statistika</a>
                                 </li>
                             </ol>
                         </div>
                     </div>
                 </div>
-                <div class="bg-green-300">
-                    <div class="flex flex-row"></div>
+                <div class="w-1/3 flex flex-col items-center">
+                    <h2 class="font-open font-bold text-lg text-white pb-4">Kontak Kami</h2>
+                    <ol>
+                        <li class="font-open font-normal text-center mb-2">
+                            <a href="tel:0247474754"
+                                class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">Tel: (024)
+                                7474754</a>
+                        </li>
+                        <li class="font-open font-normal text-center">
+                            <a href="mailto:tpmf.fsm@live.undip.ac.id"
+                                class="text-[#8996a0] hover:text-white hover:underline underline-offset-4">tpmf.fsm@live.undip.ac.id</a>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+
+            <div class="flex flex-row px-16">
+                <div class="w-1/3 flex flex-col justify-end">
+                    <p class="text-[#8996a0]">© 2023 Universitas Diponegoro. All rights reserved</p>
+                </div>
+                <div class="w-1/3 flex flex-col items-center">
+                    <img src='{{ asset('assets/img/Logo-Standar-Footer-Unit-Edit.png') }}' alt="Logo"
+                        class="max-w-[200px]">
+                </div>
+                <div class="w-1/3 flex flex-col justify-end">
+                    <div class="flex flex-row justify-evenly">
+                        <a href="https://www.facebook.com/undip.official/" target="_blank"
+                            class= "text-[#8996a0] cursor-pointer hover:text-white hover:underline underline-offset-4">Facebook</a>
+                        <a href="https://twitter.com/undip" target="_blank"
+                            class= "text-[#8996a0] cursor-pointer hover:text-white hover:underline underline-offset-4">Twitter</a>
+                        <a href="https://www.instagram.com/undip.official/" target="_blank"
+                            class= "text-[#8996a0] cursor-pointer hover:text-white hover:underline underline-offset-4">Instagram</a>
+                    </div>
                 </div>
             </div>
         </div>
-        {{-- <div class="mb-4">
-            <img src='{{asset('assets/Logo_white.svg')}}' alt="Logo" class="h-4" />
-        </div>
-        <div class="flex mb-8">
-            <a href="#">
-                <img src='{{asset('assets/logos/Facebook.svg')}}' alt="Facebook logo" class="mx-4" />
-            </a>
-            <a href="#">
-                <img src='{{asset('assets/logos/Youtube.svg')}}' alt="Youtube logo" class="mx-4" />
-            </a>
-            <a href="#">
-                <img src='{{asset('assets/logos/Instagram.svg')}}' alt="Instagram logo" class="mx-4" />
-            </a>
-            <a href="#">
-                <img src='{{asset('assets/logos/Twitter.svg')}}' alt="Twitter logo" class="mx-4" />
-            </a>
-        </div>
-        <div class="text-white font-open text-sm">
-            © 2023 Universitas Diponegoro. All rights reserved
-        </div> --}}
     </section>
