f0172e26a58c8b7f5276e253efce98d1387d2003
atikarahmanda <79711608+atikarahmanda@users.noreply.github.com>
2023-10-21 21:46:46 +0700

    ui color blue or white all done

--- resources/views/feedback_survei/index.blade.php
+++ resources/views/feedback_survei/index.blade.php
@@ -1,38 +1,3 @@
-@if (Auth::guard('mahasiswa')->check() ||
-        Auth::guard('dosen')->check() ||
-        Auth::guard('tendik')->check() ||
-        Auth::guard('tpmf')->check() ||
-        Auth::guard('dekan')->check() ||
-        Auth::guard('wadek')->check() ||
-        Auth::guard('gpm')->check())
-    <section id="tindak-lanjut" class="sectionSize bg-[#1f2f5f] pt-32">
-        <div>
-            <h2 class="secondaryTitle text-white bg-underline3 bg-100%">Upaya Tindak Lanjut dan Perbaikan</h2>
-        </div>
-        <div class="mb-6" style="color: white">
-            <h3>
-                <a href="{{ route('feedbackDosen') }}">
-                    1. <span class="hover:text-white hover:underline underline-offset-4">Upaya Tindak Lanjut dan
-                        Perbaikan
-                        Hasil Survei Kepuasan Dosen</span>
-                </a><br>
-                <a class="hover:text-white hover:underline underline-offset-4" href="{{ route('feedbackTendik') }}">
-                    2. Upaya Tindak Lanjut dan Perbaikan Hasil Survei Kepuasan Tenaga Kependidikan
-                </a><br>
-                <a class="hover:text-white hover:underline underline-offset-4" href="{{ route('feedbackMahasiswa') }}">
-                    3. Upaya Tindak Lanjut dan Perbaikan Hasil Survei Kepuasan Mahasiswa
-                </a><br>
-                <a class="hover:text-white hover:underline underline-offset-4" href="{{ route('feedbackMitra') }}">
-                    4. Upaya Tindak Lanjut dan Perbaikan Hasil Survei Kepuasan Mitra Kerjasama
-                </a><br>
-                <a class="hover:text-white hover:underline underline-offset-4"
-                    href="{{ route('feedbackStakeHolder') }}">
-                    5. Upaya Tindak Lanjut dan Perbaikan Hasil Survei Kepuasan Pengguna Lulusan
-                </a><br>
-            </h3>
-        </div>
-    </section>
-@else
     <section id="tindak-lanjut" class="sectionSize bg-white pt-32">
         <div>
             <h2 class="secondaryTitle bg-underline3 bg-100%">Upaya Tindak Lanjut dan Perbaikan</h2>
@@ -60,4 +25,3 @@
             </h3>
         </div>
     </section>
-@endif
