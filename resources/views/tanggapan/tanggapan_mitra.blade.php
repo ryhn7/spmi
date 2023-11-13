cb45813fb8aa6364b418ef5d8f899ff1165cc4c7
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-13 15:27:13 +0700

    refactor: done tanggapan mitra

--- resources/views/tanggapan/tanggapan_mitra.blade.php
+++ resources/views/tanggapan/tanggapan_mitra.blade.php
@@ -5,17 +5,31 @@
         <div><br><br>
             <h2 class="secondaryTitle bg-underline3 bg-100%" style="text-align: center;">Tanggapan Terhadap Survei Mitra
                 Kerjasama</h2>
-                @if ((Auth::guard('tpmf')->check() && $ketua) || (Auth::guard('dekan')->check() || Auth::guard('wadek')->check()))
-                    <a href="/Feedbackmitra"
-                        class="flex items-center p-3 text-base text-white rounded-lg bg-[#1f2f5f] hover:bg-[#324c99] group hover:shadow">
-                        <span class="flex-1 ml-3 whitespace-nowrap">Tambahkan tanggapan</span>
-                    </a>
-                    <br>
+            @if (Auth::guard('dekan')->check() || Auth::guard('wadek')->check())
+                @if ($feedbackDekan->toArray() != null)
                     <a href="{{ route('tanggapanmitra.edit', ['aktor' => $roleAktor]) }}"
                         class="flex items-center p-3 text-base text-white rounded-lg bg-[#1f2f5f] hover:bg-[#324c99] group hover:shadow">
                         <span class="flex-1 ml-3 whitespace-nowrap">Edit tanggapan</span>
                     </a>
+                @else
+                    <a href="/Feedbackmitra"
+                        class="flex items-center p-3 text-base text-white rounded-lg bg-[#1f2f5f] hover:bg-[#324c99] group hover:shadow">
+                        <span class="flex-1 ml-3 whitespace-nowrap">Tambahkan tanggapan</span>
+                    </a>
                 @endif
+            @elseif (Auth::guard('tpmf')->check() && $ketua)
+                @if ($feedbackTpmf->toArray() != null)
+                    <a href="{{ route('tanggapanmitra.edit', ['aktor' => $roleAktor]) }}"
+                        class="flex items-center p-3 text-base text-white rounded-lg bg-[#1f2f5f] hover:bg-[#324c99] group hover:shadow">
+                        <span class="flex-1 ml-3 whitespace-nowrap">Edit tanggapan</span>
+                    </a>
+                @else
+                    <a href="/Feedbackmitra"
+                        class="flex items-center p-3 text-base text-white rounded-lg bg-[#1f2f5f] hover:bg-[#324c99] group hover:shadow">
+                        <span class="flex-1 ml-3 whitespace-nowrap">Tambahkan tanggapan</span>
+                    </a>
+                @endif
+            @endif
             <br>
             <table class="shadow-lg bg-white">
                 <tr>
@@ -28,8 +42,8 @@
                     <tr>
                         <td class="border border-black text-center mt-2">{{ $nomor }}.</td>
                         <td class="border-b border-black px-3 py-3">{{ $pernyataan->{$nomor} }}</td>
-                        <td class="border-b border-black px-3 py-3">{!!$feedbackTpmf->{$nomor}!!}</td>
-                        <td class="border-b border-r border-black px-3 py-3">{!!$feedbackDekan->{$nomor}!!}</td>
+                        <td class="border-b border-black px-3 py-3">{!! $feedbackTpmf->{$nomor} !!}</td>
+                        <td class="border-b border-r border-black px-3 py-3">{!! $feedbackDekan->{$nomor} !!}</td>
                     </tr>
                 @endfor
             </table>
