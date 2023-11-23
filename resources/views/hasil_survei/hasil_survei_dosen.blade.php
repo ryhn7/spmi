0b46ab4964e12bdba8ad808ccaa2599eda5b7125
hanif nurul fahmi <89021658+hanifnfahmi@users.noreply.github.com>
2023-11-23 12:21:50 +0700

    coba ui button export excel

--- resources/views/hasil_survei/hasil_survei_dosen.blade.php
+++ resources/views/hasil_survei/hasil_survei_dosen.blade.php
@@ -5,11 +5,6 @@
         <br><br><br>
         {{-- @dd($final); --}}
         {{-- @dd($results); --}}
-        <form id="excel" action="/cetak-excel-dosen" method="POST">
-            @csrf
-            <input type="hidden" name="excel" value="{{ json_encode($final) }}">
-            <button type="submit">Export excel</button>
-        </form>
 
         <div class="container">
             <div class=" flex justify-center text-2xl mt-10">
@@ -32,19 +27,29 @@
                     </div>
                 @endif
             </div>
-            <form id="filter" action="/hasildosen/filter" class="py-0.5" method="GET">
-                <label class="mt-3">Tahun:</label>
-                <label for="tahun" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    <select name="tahun" id="tahun"
-                        class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option value="">Pilih Tahun</option>
-                        @foreach ($uniqueYears as $year)
-                            <option value="{{ $year }}" {{ request('tahun') == $year ? 'selected' : '' }}>
-                                {{ $year }}</option>
-                        @endforeach
-                    </select>
-                </label>
-            </form>
+            <div class="flex items-center justify-between">
+                <form id="filter" action="/hasildosen/filter" class="py-0.5" method="GET" style="width: 75%;">
+                    <label class="mt-3">Tahun:</label>
+                    <label for="tahun" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        <select name="tahun" id="tahun"
+                            class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option value="">Pilih Tahun</option>
+                            @foreach ($uniqueYears as $year)
+                                <option value="{{ $year }}" {{ request('tahun') == $year ? 'selected' : '' }}>
+                                    {{ $year }}</option>
+                            @endforeach
+                        </select>
+                    </label>
+                </form>
+                <form id="excel" action="/cetak-excel-dosen" method="POST" style="width: 15%;">
+                    @csrf
+                    <input type="hidden" name="excel" value="{{ json_encode($final) }}">
+                    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" type="submit">
+                        <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                        <span style="font-size: 12px">Download Excel</span>
+                    </button>
+                </form>
+            </div>
             <label class="mt-3 text-center justify-center">Total Data: {{ $totalData }}</label>
             <div class="container-fluid mt-10 mx-auto">
                 <div class="select-none rounded-lg border border-gray-100 p-6 shadow-lg ">
