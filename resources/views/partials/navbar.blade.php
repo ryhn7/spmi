9469c07a346697597bf30bdb674065401c2f51d2
atikarahmanda <79711608+atikarahmanda@users.noreply.github.com>
2023-11-23 12:58:52 +0700

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -40,6 +40,21 @@
         <li class="font-medium growing-underline mx-3">
             <a href="{{ route('dashboard') }}#tindak-lanjut">Tindak Lanjut</a>
         </li>
+        <li class="font-medium growing-underline mx-3">
+            @if (Auth::guard('dekan')->check() || Auth::guard('wadek')->check())
+                <a href="/surveiTendik"> Cara Penggunaan </a>
+            @elseif (Auth::guard('kaprodi')->check())
+                <a href="/surveiDsn"> Cara Penggunaan </a>
+            @elseif (Auth::guard('dosen')->check())
+                <a href="/surveiDsn"> Cara Penggunaan </a>
+            @elseif (Auth::guard('tpmf')->check())
+                <a href="/surveiDsn"> Cara Penggunaan </a>
+            @elseif (Auth::guard('gpm')->check())
+                <a href="/surveiDsn"> Cara Penggunaan </a>
+            @elseif (Auth::guard('mahasiswa')->check())
+                <a href="/surveiMhs"> Cara Penggunaan </a>
+            @endif
+        </li>
     </ul>
 
 
